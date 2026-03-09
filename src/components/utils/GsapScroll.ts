4d36d05f6aa018c9047403bf8159cde3dab8c483
unknown <prashantkumarsheoganj@gmail.com>
2026-03-09 14:56:42 +0000

    update

--- src/components/utils/GsapScroll.ts
+++ src/components/utils/GsapScroll.ts
@@ -120,6 +120,61 @@ export function setCharTimeline(
     }
   } else {
     if (character) {
+      tl1
+        .fromTo(character.rotation, { y: 0 }, { y: 0.7, duration: 1 }, 0)
+        .to(camera.position, { z: 40 }, 0)
+        .fromTo(".character-model", { x: 0 }, { x: "0%", duration: 1 }, 0)
+        .to(".landing-container", { opacity: 0, duration: 0.4 }, 0)
+        .to(".landing-container", { y: "20%", duration: 0.8 }, 0)
+        .fromTo(".about-me", { y: "-20%" }, { y: "0%" }, 0);
+
+      tl2
+        .to(
+          camera.position,
+          { z: 90, y: 15, duration: 6, delay: 2, ease: "power3.inOut" },
+          0
+        )
+        .to(".about-section", { y: "15%", duration: 6 }, 0)
+        .to(".about-section", { opacity: 0, delay: 3, duration: 2 }, 0)
+        .fromTo(
+          ".character-model",
+          { pointerEvents: "inherit" },
+          { pointerEvents: "none", x: "0%", delay: 2, duration: 5 },
+          0
+        )
+        .to(character.rotation, { y: 0.92, x: 0.12, delay: 3, duration: 3 }, 0)
+        .to(neckBone!.rotation, { x: 0.6, delay: 2, duration: 3 }, 0)
+        .to(monitor.material, { opacity: 1, duration: 0.8, delay: 3.2 }, 0)
+        .to(screenLight.material, { opacity: 1, duration: 0.8, delay: 4.5 }, 0)
+        .fromTo(
+          ".what-box-in",
+          { display: "none" },
+          { display: "flex", duration: 0.1, delay: 6 },
+          0
+        )
+        .fromTo(
+          monitor.position,
+          { y: -10, z: 2 },
+          { y: 0, z: 0, delay: 1.5, duration: 3 },
+          0
+        )
+        .fromTo(
+          ".character-rim",
+          { opacity: 1, scaleX: 1.4 },
+          { opacity: 0, scale: 0, y: "-40%", duration: 5, delay: 2 },
+          0.3
+        );
+
+      tl3
+        .fromTo(
+          ".character-model",
+          { y: "0%" },
+          { y: "-60%", duration: 4, ease: "none", delay: 1 },
+          0
+        )
+        .fromTo(".whatIDO", { y: 0 }, { y: "10%", duration: 2 }, 0)
+        .to(character.rotation, { x: -0.04, duration: 2, delay: 1 }, 0);
+
       const tM2 = gsap.timeline({
         scrollTrigger: {
           trigger: ".what-box-in",
